953076e1daa863056fc6b1de3c32d3d1ddcf92e6
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-01-28 23:25:43 -0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
